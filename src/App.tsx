46868c73919774ef893007617bff45a05bdb63af
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-05-27 19:03:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Careers from "./pages/careers";
+import Careers from "./pages/Careers";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Services from "./pages/Services";
