932a302f0bec37a60a40926774c0e5c5e21e3a48
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-06-02 16:00:42 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -72,7 +72,11 @@ const About = () => {
           {/* Background with overlay */}
           <div className="absolute inset-0 bg-gradient-to-br from-blue-900 via-blue-700 to-indigo-800">
             <div className="absolute inset-0 bg-black/20"></div>
-            <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.05"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30"></div>
+            <div className="absolute inset-0 opacity-30">
+              <div className="w-full h-full" style={{
+                backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.05'%3E%3Ccircle cx='30' cy='30' r='2'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`
+              }}></div>
+            </div>
           </div>
           
           <div className="relative z-10 text-center px-4 max-w-5xl mx-auto">
