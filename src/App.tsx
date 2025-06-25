274b1f6c9663330b3b90ef554c6ed11d8d44735e
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-06-25 12:51:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Careers from "./pages/Careers";
 import Contact from "./pages/Contact";
@@ -35,6 +35,9 @@ function App() {
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
+            {/* Redirect routes */}
+            <Route path="/india" element={<Navigate to="/" replace />} />
+            <Route path="/index" element={<Navigate to="/" replace />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
