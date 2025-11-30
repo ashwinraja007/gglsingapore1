cae7c79a0665439cd612e8c679cd9fcf04ec8c36
ashwinraja007 <ashwinraja007@gmail.com>
2025-11-30 18:33:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import LCLConsolidation from "./pages/services/LCLConsolidation";
 import Warehousing from "./pages/services/Warehousing";
 import ProjectCargo from "./pages/services/ProjectCargo";
 import BangladeshHome from "./pages/BangladeshHome";
-import CustomsClearance from "./pages/CustomsClearance";
+import CustomsClearance from "./pages/services/CustomsClearance";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import GlobalPresence from "./pages/GlobalPresence";
 import { ScrollToTop } from "./components/common/ScrollToTop";
