81219736bd8bf32b42b99fcb9fcc3869652e62bb
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-02-15 19:17:19 +0530

    Update BCountrySelector.tsx

--- src/components/common/BCountrySelector.tsx
+++ src/components/common/BCountrySelector.tsx
@@ -19,7 +19,7 @@ interface CountryData {
 }
 
 const countries: CountryData[] = [
-  { country: "MALAYSIA", company: "OECL", website: "https://www.oecl.sg/malaysia/home", priority: 3, flag: "/my.svg" },
+  { country: "MALAYSIA", company: "GGL", website: "https://www.ggl.sg/malaysia/home", priority: 3, flag: "/my.svg" },
   { country: "SINGAPORE", company: "GGL", website: "https://ggl.sg/", priority: 1, flag: "/sg.svg" },
   { country: "INDONESIA", company: "OECL", website: "https://www.oecl.sg/indonesia/home", priority: 6, flag: "/id.svg" },
   { country: "THAILAND", company: "OECL", website: "https://www.oecl.sg/thailand/home", priority: 4, flag: "/th.svg" },
